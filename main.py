e183da2edfc696e55cd983fb49aff8d3f60e559a
Widitra <widitra.r@gmail.com>
2022-11-07 17:49:45 +0700

    updated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -16,12 +20,14 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-screen.exitonclick()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.score = 0
+        self.penup()
+        self.goto(0, 265)
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"SCORE: {self.score}", align="center", font=("Courier", 20, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()

a96278eafcf239b5a43cf24c0522f5337cdcd135
Widitra <widitra.r@gmail.com>
2022-11-15 13:49:03 +0700

    updated

--- main.py
+++ main.py
@@ -28,12 +28,16 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.game_over()
         game_is_on = False
 
-
+    for segments in snake.segments[1:]:
+        if snake.head.distance(segments) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -17,11 +17,17 @@ class Snake:
 
     def create_snake(self):
         for position in turtle_position:
-            new_turtle = Turtle("square")
-            new_turtle.color("white")
-            new_turtle.penup()
-            new_turtle.goto(position)
-            self.segments.append(new_turtle)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_turtle = Turtle("square")
+        new_turtle.color("white")
+        new_turtle.penup()
+        new_turtle.goto(position)
+        self.segments.append(new_turtle)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
